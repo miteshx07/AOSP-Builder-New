4998a2a35f9420bfc06b61edf7256bf91065c447
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-04-12 22:52:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,11 +20,11 @@ lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
 
 # Build the Code
 if [ -z "$J_VAL" ]; then
-    mka -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+    make -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
 elif [ "$J_VAL"="0" ]; then
-    mka $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+    make $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
 else
-    mka -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+    make -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
 fi
 
 # Exit
